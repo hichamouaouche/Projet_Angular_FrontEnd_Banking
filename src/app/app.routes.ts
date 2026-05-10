4b3ab5cda007b63497f14a09802b46430bee3a28
hichamouaouche <hichamouaouche@gmail.com>
2026-05-10 19:44:53 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './customers/customers'; // Ajoute le chemin complet
+import { AccountsComponent } from './accounts/accounts';   // Idem ici
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "customers", component: CustomersComponent },
+  { path: "accounts", component: AccountsComponent },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Assure-toi que standalone est présent
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
