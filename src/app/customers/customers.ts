7fa2a882ea3bbc3afc5da2fea55aabad2bc6bf61
hichamouaouche <hichamouaouche@gmail.com>
2026-05-11 07:16:24 +0100

    update

--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  // On ajoute CommonModule pour pouvoir utiliser *ngFor et *ngIf dans le HTML
+  imports: [CommonModule],
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
-export class CustomersComponent {}
+export class CustomersComponent implements OnInit {
+  // Variable pour stocker la liste des clients
+  customers: any;
+
+  // Injection du service HttpClient
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    // Appel au backend lors du chargement du composant
+    this.http.get("http://localhost:8085/customers").subscribe({
+      next: (data) => {
+        this.customers = data;
+      },
+      error: (err) => {
+        console.error("Erreur de récupération :", err);
+      }
+    });
+  }
+}
